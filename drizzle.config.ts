6b2de3105fe4ccb9c159f7e107f700048cc088b8
Erik <erik.bg7@gmail.com>
2025-05-13 14:49:48 +0200

    chore(db): script to seed database with fake data

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+console.log('DATABASE_URL', process.env.DIRECT_DATABASE_URL);
+
+if (!process.env.DIRECT_DATABASE_URL)
+	throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/server/db/schema.ts',
 
 	dbCredentials: {
-		url: process.env.DATABASE_URL,
+		url: process.env.DIRECT_DATABASE_URL,
 	},
 
 	verbose: true,
--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -6,3 +6,5 @@ export async function seedDatabase() {
 	console.log('Seeding database...');
 	await seed(db, { profile });
 }
+
+seedDatabase();
