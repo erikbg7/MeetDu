d90590dfa34cd24970ebe9eae378bdeebc572c00
Erik <erik.bg7@gmail.com>
2025-05-14 10:16:12 +0200

    feat(db): schema to contain github id and number of repos

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,16 +1,19 @@
+import { getTableColumns } from 'drizzle-orm';
 import { pgTable, text, timestamp, integer, index } from 'drizzle-orm/pg-core';
 
-export const profile = pgTable(
+const profile = pgTable(
 	'profile',
 	{
-		id: integer('id').primaryKey().notNull(),
+		id: text('id').primaryKey().notNull(),
 		name: text('name').notNull(),
+		githubId: integer('github_id').notNull(),
 		username: text('username').unique().notNull(),
 		url: text('url').notNull(),
 		location: text('location').notNull(),
 		bio: text('bio').notNull(),
 		avatar: text('avatar'),
-		karma: integer('karma').notNull(),
+		karma: integer('karma').notNull().default(0),
+		repos: integer('repos').notNull().default(0),
 
 		createdAt: timestamp('created_at', { withTimezone: true })
 			.notNull()
@@ -18,3 +21,13 @@ export const profile = pgTable(
 	},
 	(table) => [index('karma_idx').on(table.karma.desc())],
 );
+
+// eslint-disable-next-line
+const { id, createdAt, ...publicProfileColumns } = getTableColumns(profile);
+
+export type Profile = typeof profile.$inferSelect;
+export type ProfileInsert = typeof profile.$inferInsert;
+export type ProfileUpdate = Partial<typeof profile.$inferInsert>;
+export type PublicProfile = Omit<Profile, 'id' | 'createdAt'>;
+
+export { profile, publicProfileColumns };
