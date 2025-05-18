44a0b228cc5c4f951abe435f2dde5ec43c75dd75
Erik <erik.bg7@gmail.com>
2025-05-18 09:36:21 +0200

    feat(ui): add provisional logo in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,12 +5,16 @@ import {
 	SignUpButton,
 	UserButton,
 } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export function NavBar() {
 	return (
 		<header className="supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 h-16 w-full gap-4 border-b backdrop-blur">
 			<div className="container mx-auto flex h-16 w-full items-center justify-between p-4">
-				<div className="text-xl font-bold">MeetDu</div>
+				<div className="flex items-center text-xl font-bold">
+					<Image src="/meetdu.png" alt="Logo" width={48} height={48} />
+					<span>MeetDu</span>
+				</div>
 				<div>
 					<SignedOut>
 						<SignInButton />
