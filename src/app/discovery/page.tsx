19a3d8cb851939e1584a11939bf08db250d67e0e
Erik <erik.bg7@gmail.com>
2025-05-21 00:56:14 +0200

    feat(ui): add empty users list message in discovery

--- src/app/discovery/page.tsx
+++ src/app/discovery/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 export const dynamic = 'force-dynamic'; // allows headers, disables SSG
 
 import { GetTopUsersError, SyncUserError } from '@/lib/errors';
@@ -16,6 +18,20 @@ export default async function DiscoveryPage() {
 
 	const users = await getTopUsers();
 	if (!users.success) throw new GetTopUsersError();
+
+	if (users.data.length === 0) {
+		return (
+			<div className="container mx-auto flex h-[50vh] flex-col items-center justify-center">
+				<h2 className="text-xl font-semibold">
+					Have you really followed everyone?
+				</h2>
+				<p className="text-muted-foreground">
+					Can't find more users to follow at this moment. Try again in a while
+					:P
+				</p>
+			</div>
+		);
+	}
 
 	return (
 		<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
